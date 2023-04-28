929e1369e1cf4925e8b0a09a77a25e0028b87c56
Nathan Spencer <nathantspencer@gmail.com>
2023-04-28 10:47:25 -0400

    Grabs user-indicated channel

--- channel_to_mask.py
+++ channel_to_mask.py
@@ -14,15 +14,19 @@ def main(project_path: str, channel_number: int):
     max_resolution_level = len(resolution_map) - 1
     block_count = resolution_map[max_resolution_level]
 
+    # does not consider timeseries data for now
     for i in range(block_count):
-        # does not consider timeseries data for now
-        block = project.get_block(0, max_resolution_level, i)
         
-				# TODO: consider selected channel, add as mask
+        block = project.get_block(0, max_resolution_level, i)
+        print(block.data.shape)
+        channel_block = block.data[:, :, :, channel_number - 1]
+        
+				# TODO: consider threshold, add mask
+
 
 if __name__ == "__main__":
     
     if len(sys.argv) != 3:
         print("Usage: python channel_to_mask.py [path/to/project.syg] [channel_number]")
     else:
-        main(sys.argv[1], sys.argv[2])
+        main(sys.argv[1], int(sys.argv[2]))
